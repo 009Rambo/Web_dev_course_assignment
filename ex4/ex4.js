05d49ea0873a26ff09ba33eb6b02d6dc013bb40a
Ramesh Pandey <ramesh.pandey@tuni.fi>
2023-02-05 23:55:39 +0200

    ex4 commit final ready

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,10 +1,6 @@
-
 function task5(a, b, p) {
     return Number((a / b).toFixed(p));
   }
-
-
-
 
 const getEncryptor = (alphabet, shift1, shift2, shift2freq) => {
 
@@ -19,16 +15,17 @@ const getEncryptor = (alphabet, shift1, shift2, shift2freq) => {
       }
 
       let shifted2 = '';
-      if  (shift1 < 0) {
+      if  (shift2 < 0) {
         shifted2 = alphabet.substring(Math.abs(shift2), alphabet.length) + alphabet.substring(0, Math.abs(shift2)) ;
       } else {
         shifted2 = alphabet.substring(alphabet.length - shift2, alphabet.length) + alphabet.substring(0, alphabet.length - 1);
       }
 
-      for (c in message) {
-        let ch = message[c];
+      for (let i = 0; i < message.length; i++) {
+        let ch = message[i];
         let ch_index = alphabet.indexOf(ch);
-        if (c + 1 % shift2freq === 0 && c !== 0) {
+        if ((i + 1 ) % shift2freq === 0 && i !== 0) {
+          let t = shifted2.charAt(ch_index)
           encoded += shifted2.charAt(ch_index);
         } else {
           encoded += shifted1.charAt(ch_index);
@@ -41,6 +38,5 @@ const getEncryptor = (alphabet, shift1, shift2, shift2freq) => {
   const abc = '0123456789abcdefghijklmnopqrstuvwxyz ';
   const encoded = getEncryptor(abc, -1, 3, 4)
   console.log(encoded('javascript'));
-
   const result = task5(3,2,2);
-  console.log(result)
+  console.log(result);
