9eb97275257fb4e8df2894d3d1cd338aa25128ca
Ramesh Pandey <ramesh.pandey@tuni.fi>
2023-03-29 23:59:39 +0300

    ex10 finally ready

--- ex10/server.js
+++ ex10/server.js
@@ -8,7 +8,10 @@ const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 connection.connect((err) => {
